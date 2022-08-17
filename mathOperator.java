223c271ff6eb7c42932e33b1e526fe58ef941931
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2022-08-16 23:48:42 -0700

    Create mathOperator.java

---
+++ mathOperator.java
@@ -0,0 +1,12 @@
+//import java.util.Scanner;
+public class apples {
+	public static void main(String args[]) {
+		//Scanner variable = new Scanner(System.in);
+
+		int boys, girls, people;
+		boys = 5;
+		girls = 9;
+		people = girls % boys;
+		System.out.println(people);
+	}
+}
